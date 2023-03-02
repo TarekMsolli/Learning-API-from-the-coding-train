377b1d2de327e367c440e1040cbac025aa0b2831
Tarek <tarekmsolli@gmail.com>
2023-03-02 01:00:59 +0100

    API's are cool

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const mymap = L.map('issMap').setView([0, 0], 1);
+
+const issIcon = L.icon({
+    iconUrl: 'images/iss.svg',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16]
+});
+
+const marker = L.marker([0, 0], {icon: issIcon}).addTo(mymap);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles =   L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+async function getISS(){
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const { latitude, longitude } = data;
+
+    marker.setLatLng([latitude, longitude]);
+    document.getElementById('lat').textContent = latitude;
+    document.getElementById('lon').textContent = longitude;
+}
+setInterval(getISS,1000);
